d97376615d1e1b3ab48ea92a5658b076106e31eb
Safoora <safooraqasimi2@gmail.com>
2024-08-05 17:06:01 -0400

    Created Selenium Utilities Class

--- src/test/java/Utility/SeleniumUtilities.java
+++ src/test/java/Utility/SeleniumUtilities.java
@@ -2,15 +2,20 @@ package Utility;
 
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import tek.bdd.base.BaseSetup;
 import java.time.Duration;
-public class SeleniumUtility extends BaseSetup {
-    private WebDriverWait getWait(){ return new WebDriverWait(getDriver(), Duration.ofSeconds(20)); }
-    create a method to click on a given Locator
-     public void clickOnElement(By locator) { getWait().until(ExpectedConditions.elementToBeClickable(locator)) .click();
+public class SeleniumUtilities extends BaseSetup {
+    private WebDriverWait getWait(){ return new WebDriverWait(getDriver(),
+            Duration.ofSeconds(20));}
+   // create a method to click on a given Locator
+     public void clickOnElement(By locator) {
+        getWait().until(ExpectedConditions.elementToBeClickable(locator)).click();
      }
-    /public void sendText(By locator, String value) { getWait().until(ExpectedConditions.visibilityOfElementLocated(locator)) .sendKeys(value); }
+    public void sendText(By locator, String value)
+    { getWait().until(ExpectedConditions.visibilityOfElementLocated(locator)).sendKeys(value);
+    }
 }
+//create method fr getting the text of a locator
+
